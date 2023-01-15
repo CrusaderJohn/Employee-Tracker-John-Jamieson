28b389c8ca2711228833deef722409f84b2eaebd
John Jamieson <jamieson.john@gmail.com>
2023-01-15 13:43:23 -0500

    Added menu functions

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 const mainMenuList = ["View All Departments", "View All Roles", "View All Employees",
-    "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"];
+    "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Finished"];
 
 const questions = [
     {
         type: 'list',
         message: "What would you like to do?",
-        name: 'mainMenu',
+        name: 'mainMenuQuestion',
         choices: mainMenuList,
     },
 ];
@@ -24,8 +24,70 @@ const database = mysql.createConnection(
     console.log(`Connected to the employeeTrackerDB database.`)
 );
 
+function viewDepartments() {
+
+}
+
+function viewRoles() {
+
+}
+
+function viewEmployees() {
+
+}
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function addEmployee() {
+
+}
+
+function updateEmployee() {
+
+}
+
+function mainMenuFunction() {
+    inquirer.prompt(questions).then((answers) => {
+        switch (answers.mainMenuQuestion) {
+            case mainMenuList[0]:
+                day = "Sunday";
+                mainMenuFunction();
+                break;
+            case mainMenuList[1]:
+                day = "Monday";
+                mainMenuFunction();
+                break;
+            case mainMenuList[2]:
+                day = "Tuesday";
+                mainMenuFunction();
+                break;
+            case mainMenuList[3]:
+                day = "Wednesday";
+                mainMenuFunction();
+                break;
+            case mainMenuList[4]:
+                day = "Thursday";
+                mainMenuFunction();
+                break;
+            case mainMenuList[5]:
+                day = "Friday";
+                mainMenuFunction();
+                break;
+            case mainMenuList[6]:
+                day = "Saturday";
+                mainMenuFunction();
+                break;
+            default:
+                console.log("Goodbye!");
+        }
+    });
+}
+
 console.log("Welcome to the Employee Management System.");
-inquirer.prompt(questions).then((answers) => {
-    // console.log('\nOrder receipt:');
-    // console.log(JSON.stringify(answers, null, '  '));
-});
+mainMenuFunction();
